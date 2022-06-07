fd2e865405e64acb80b9e0748fadf05119d6fe40
Jane <gigimcc4@gmail.com>
2022-06-07 12:21:44 -0400

    <update lab 2>

--- foundation_lab_2/reading_data_codealong.R
+++ foundation_lab_2/reading_data_codealong.R
@@ -1,11 +1,11 @@
-###############################################################################
-#########################  Reading In Data   ##################################
-###############################################################################
+##############################
+#####  Reading In Data   #####
+##############################
 
-#Continuing on from foundation lab one we will use "good Programming practices" to learn more about reading in data.
+# Continuing on from foundation lab one we will use "good Programming practices" to learn more about reading in data.
 
-# Again we are using a script file in R Studio. You will become fmiliar with R Mardown files later in the day or week. In order to read in data we must load packages that allow us to read data in.
+# Again we are using a script file in R Studio. You will become familiar with R Markdown files later in the day or week. In order to read in data we must load packages that allow us to read data in.
 
-# R comes with several packages when you start a session. Packages are a collection of objects, data sets around a theme or purpose. You may find a package whose objects are for the putpose of correlation, like in the cor package. Or you may find a package that has objects to produce better tables, like in the kableextra package. Or you may find a package that will help wrangling the data (processing it) easier than in Base R package. That is the package we are going to install here Tidyverse.
+# R comes with several packages when you start a session. Packages are a collection of objects, data sets around a theme or purpose. You may find a package whose objects are for the purpose of correlation, like in the cor package. Or you may find a package that has objects to produce better tables, like in the *kable extra* package. Or you may find a package that will help wrangling the data (processing it) easier than in Base R package. That is the package we are going to install here Tidyverse.
 
-#Tidyverse is a suite of packages that share a common philosophy and are designed to work together. Think Google products or Microscoft products. data importing, data wrangling and plotting.
+#Tidyverse is a suite of packages that share a common philosophy and are designed to work together. Think Google products or Microsoft products. data importing, data wrangling and plotting.
